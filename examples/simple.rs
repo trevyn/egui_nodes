e5974bab8ae544bd97a4010fa48397c1d6450270
trevyn <230691+trevyn@users.noreply.github.com>
2022-10-16 10:35:12 +0400

    update example for new eframe constructor

--- examples/simple.rs
+++ examples/simple.rs
@@ -61,6 +61,16 @@ pub fn example_graph(ctx: &mut Context, links: &mut Vec<(usize, usize)>, ui: &mu
     }
 }
 
+impl MyApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
 impl App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -77,6 +87,6 @@ fn main() {
     eframe::run_native(
         "My egui App",
         eframe::NativeOptions::default(),
-        Box::new(|_cc| Box::new(MyApp::default())),
+        Box::new(|cc| Box::new(MyApp::new(cc))),
     );
 }
